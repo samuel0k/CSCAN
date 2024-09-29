9fe02268060a6492e22d908ad3e0364183352d4a
samuel0k <ceifmode@gmail.com>
2024-09-29 14:45:42 -0300

    Atualiza código do cscan para corrigir erro de IP

--- cscan.c
+++ cscan.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h> // Para usar strcspn
 
 // Cores ANSI
 #define RESET   "\033[0m"
-#define ROXO    "\033[35m"  // Cor roxa
-#define LARANJA "\033[38;5;214m" // Cor laranja
 #define VERDE   "\033[32m"   // Cor verde
-#define AZUL    "\033[34m"   // Cor azul
 
 void print_logo() {
-    printf(ROXO "⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣶⣶⣶⣤⣤⣤⣤⣤⣤⣤⣶⣶⣶⣶⣶⣶⣦⣤⣤⡀\033[38;5;214m⠀⣶⠀\n" RESET);
-    printf(ROXO "⢸⣿⣿⣿⣿⣿⣿⡿⢿⣿⡿⠟⢻⣿⣿⡟⠛⠛⠛⠉⠙⠛⠋⠀⠈⠉⠀⠈⠉⠁\n" RESET);
-    printf(ROXO "⢸⣿⠿⠟⠋⠉⠀⢀⣾⡿⠉⠀⠈⠸⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" RESET);
-    printf(ROXO "⠀⠀⠀⠀⠀⠀⠀⠀⠉⠁⠀⠀⠀⠀⠹⣿⣷⣤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" RESET);
-    printf(ROXO "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" RESET);
+    printf(VERDE "⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣶⣶⣶⣤⣤⣤⣤⣤⣤⣤⣶⣶⣶⣶⣶⣶⣦⣤⣤⡀\n" RESET);
+    printf(VERDE "⢸⣿⣿⣿⣿⣿⣿⡿⢿⣿⡿⠟⢻⣿⣿⡟⠛⠛⠛⠉⠙⠛⠋⠀⠈⠉⠀⠈⠉⠁\n" RESET);
+    printf(VERDE "⢸⣿⠿⠟⠋⠉⠀⢀⣾⡿⠉⠀⠈⠸⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" RESET);
+    printf(VERDE "⠀⠀⠀⠀⠀⠀⠀⠀⠉⠁⠀⠀⠀⠀⠹⣿⣷⣤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" RESET);
+    printf(VERDE "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" RESET);
 }
 
 void scan_network(const char *interface) {
@@ -23,30 +21,48 @@ void scan_network(const char *interface) {
     system(command);
 }
 
+void list_network_interfaces() {
+    printf(VERDE "Listando interfaces de rede disponíveis:\n" RESET);
+    system("ip -o link show | awk -F': ' '{print NR\": \"$2}'");
+}
+
 int main() {
     print_logo();
     printf(VERDE "Samuel Kusancke\n" RESET);
     printf(VERDE "Versão 1.0\n\n" RESET);
 
-    // Listando interfaces de rede
-    const char *interfaces[] = {"lo", "enp4s0", "wlp2s0", "wlx503eaad6d97d"};
-    int num_interfaces = sizeof(interfaces) / sizeof(interfaces[0]);
-
-    for (int i = 0; i < num_interfaces; i++) {
-        printf(AZUL "%d: %s\n" RESET, i + 1, interfaces[i]);
-    }
+    // Listar todas as interfaces de rede dinamicamente
+    list_network_interfaces();
 
     int choice;
     printf("Digite o número da interface de rede que deseja usar: ");
     scanf("%d", &choice);
-    
-    if (choice < 1 || choice > num_interfaces) {
-        printf(VERDE "Escolha inválida!\n" RESET);
+
+    // Obtenha o nome da interface baseado na escolha do usuário
+    char command[100];
+    snprintf(command, sizeof(command), "ip -o link show | awk -F': ' '{if (NR==%d) print $2}'", choice);
+
+    // Abrir o pipe para capturar a saída do comando
+    FILE *fp = popen(command, "r");
+    if (fp == NULL) {
+        printf(VERDE "Erro ao obter a interface de rede.\n" RESET);
         return 1;
     }
 
+    // Captura o nome da interface escolhida
+    char interface[20];
+    if (fgets(interface, sizeof(interface), fp) == NULL) {
+        printf(VERDE "Escolha inválida!\n" RESET);
+        pclose(fp);
+        return 1;
+    }
+    pclose(fp);
+
+    // Remover nova linha no final do nome da interface, se houver
+    interface[strcspn(interface, "\n")] = 0;
+
     // Executar o escaneamento
-    scan_network(interfaces[choice - 1]);
+    scan_network(interface);
 
     return 0;
 }
